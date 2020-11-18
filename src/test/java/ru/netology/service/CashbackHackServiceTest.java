0bee46ca6ad8e2e27755d7fea40c1b84628f19ec
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-11-18 16:45:33 +0300

    Изменение к задаче №2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,33 +5,33 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class CashbackHackerServiceTest {
-    CashbackHackerService cashbackHackerService = new CashbackHackerService();
+public class CashbackHackServiceTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
 
     @Test
     public void boundaryAmount() {
-        int actual = cashbackHackerService.remain(1000);
+        int actual = cashbackHackService.remain(1000);
         int expected = 0;
         assertEquals(actual, expected);
     }
 
     @Test
     public void lessBoundaryAmount() {
-        int actual = cashbackHackerService.remain(900);
+        int actual = cashbackHackService.remain(900);
         int expected = 100;
         assertEquals(actual, expected);
     }
 
     @Test
     public void moreBoundaryAmount() {
-        int actual = cashbackHackerService.remain(1200);
+        int actual = cashbackHackService.remain(1200);
         int expected = 800;
         assertEquals(actual, expected);
     }
 
     @Test
     public void zeroBoundaryAmount() {
-        int actual = cashbackHackerService.remain(0);
+        int actual = cashbackHackService.remain(0);
         int expected = 1000;
         assertEquals(actual, expected);
     }
